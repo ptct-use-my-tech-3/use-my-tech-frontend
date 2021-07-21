1941186d982a7dc1f004b4c90ec3c4ca98540c5b
Trevor Locke <locke.trevor@gmail.com>
2021-07-21 18:38:24 -0500

    added some comments, added a route for userhome and matching component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import UserHome from "./components/UserHome";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// TODO: uncomment when user auth is available
+// import { PrivateRoute } from "./Components/Util/PrivateRoute";
+
 function App() {
 	return (
+		// TODO: remove <Route /> for UserHome and uncomment <PrivateRoute /> when user auth is done. Only there now to be able to view UserHome and build it without having to be logged in
 		<Router>
 			<Navbar />
 			<Route path="/login" component={Login} />
 			<Route path="/signup" component={Signup} />
+			<Route path="home" component={UserHome} />
+			{/* <PrivateRoute exact path="/home">
+        <UserHome/>
+      </PrivateRoute> */}
 		</Router>
 	);
 }
---
+++ src/components/UserHome.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserHome = () => {
+	return <div>{/* home page to display if user is logged in */}</div>;
+};
+
+export default UserHome;
