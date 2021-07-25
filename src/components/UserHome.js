87d53fa379f2b8b4836e8b5f214dda69dcbdb645
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2021-07-24 19:02:08 -0700

    Added text to UserHome to make sure PrivateRoute is working.

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const UserHome = () => {
-	return <div>{/* home page to display if user is logged in */}</div>;
+	return <div>
+			<h2>User Home Page</h2>
+		{/* home page to display if user is logged in */}
+		
+			</div>;
 };
 
 export default UserHome;
